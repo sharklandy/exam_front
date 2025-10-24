73922cbf861f8a36a6b0634e2bef93836998aa9c
sharklandy <landry.lhomme@supdevinci-edu.fr>
2025-10-24 11:28:50 +0200

    feat: enhance UserCard and UserDetail components with improved styling and structure

--- exam_front/src/components/UserCard.jsx
+++ exam_front/src/components/UserCard.jsx
@@ -2,11 +2,13 @@ import { Link } from 'react-router-dom';
 
 function UserCard({ user }) {
   return (
-    <div style={{ border: '1px solid #ccc', margin: '10px', padding: '10px' }}>
+    <div className="user-card">
       <img src={user.image} alt={user.firstName} width={50} />
-      <h3>{user.firstName} {user.lastName}</h3>
-      <p>{user.email}</p>
-      <Link to={`/user/${user.id}`}>Voir détails</Link>
+      <div>
+        <h3>{user.firstName} {user.lastName}</h3>
+        <p>{user.email}</p>
+        <Link to={`/user/${user.id}`}>Voir détails</Link>
+      </div>
     </div>
   );
 }
--- exam_front/src/components/UserDetail.jsx
+++ exam_front/src/components/UserDetail.jsx
@@ -1,6 +1,6 @@
 function UserDetail({ user }) {
   return (
-    <div>
+    <div className="user-detail">
       <img src={user.image} alt={user.firstName} width={100} />
       <h2>{user.firstName} {user.lastName}</h2>
       <p>Email : {user.email}</p>
