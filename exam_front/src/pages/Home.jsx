c416707650f4b52fabea18c5dab6922ff85c2901
sharklandy <landry.lhomme@supdevinci-edu.fr>
2025-10-24 10:41:12 +0200

    feat: create Home component to display user list

---
+++ exam_front/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import UserList from '../components/UserList';
+
+function Home() {
+  return (
+    <div>
+      <h1>Liste des utilisateurs</h1>
+      <UserList />
+    </div>
+  );
+}
+
+export default Home;
