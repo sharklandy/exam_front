8e77bc6026944038ac30e85db3e245e72215f1c2
sharklandy <landry.lhomme@supdevinci-edu.fr>
2025-10-24 10:40:52 +0200

    feat: add UserDetail component to display detailed user information

---
+++ exam_front/src/componanents/UserDetail.jsx
@@ -0,0 +1,14 @@
+function UserDetail({ user }) {
+  return (
+    <div>
+      <img src={user.image} alt={user.firstName} width={100} />
+      <h2>{user.firstName} {user.lastName}</h2>
+      <p>Email : {user.email}</p>
+      <p>Âge : {user.age}</p>
+      <p>Entreprise : {user.company.name}</p>
+      <p>Ville : {user.address.city}</p>
+    </div>
+  );
+}
+
+export default UserDetail;
